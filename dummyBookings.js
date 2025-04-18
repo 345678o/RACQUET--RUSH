3ff0ac6f4ed903b01d7cf63a3110e4b03134f133
345678o <anamikakumari.snas@gmail.com>
2025-04-18 15:08:12 +0530

    Add files via upload

---
+++ dummyBookings.js
@@ -0,0 +1,55 @@
+// dummyBookings.js
+
+export const dummyBookings = [
+    {
+      bookingId: "BK1001",
+      username: "rohan123",
+      email: "rohan@example.com",
+      phone: "9876543210",
+      court: 1,
+      slot: "1:00 PM - 1:30 PM",
+      date: "2025-04-08",
+      userType: "Medical"
+    },
+    {
+      bookingId: "BK1002",
+      username: "aisha.mba",
+      email: "aisha@example.com",
+      phone: "9988776655",
+      court: 3,
+      slot: "3:00 PM - 3:30 PM",
+      date: "2025-04-08",
+      userType: "MBA"
+    },
+    {
+      bookingId: "BK1003",
+      username: "vivek.btech",
+      email: "vivek@example.com",
+      phone: "9877612345",
+      court: 5,
+      slot: "4:00 PM - 4:30 PM",
+      date: "2025-04-08",
+      userType: "BTech"
+    },
+    {
+      bookingId: "BK1004",
+      username: "meera",
+      email: "meera@example.com",
+      phone: "9123456789",
+      court: 2,
+      slot: "5:00 PM - 5:30 PM",
+      date: "2025-04-08",
+      userType: "Medical"
+    },
+    {
+      bookingId: "BK1005",
+      username: "tanya123",
+      email: "tanya@example.com",
+      phone: "9765432109",
+      court: 4,
+      slot: "6:00 PM - 6:30 PM",
+      date: "2025-04-08",
+      userType: "MBA"
+    }
+  ];
+  
